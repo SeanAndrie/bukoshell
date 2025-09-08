13e6b913f028f28bbd9e956f87a9c9245f7c99e5
SeanAndrie <gseanandrie@gmail.com>
2025-09-08 12:41:45 +0400

    fix: memory leaks in in main driver

--- src/shell.c
+++ src/shell.c
@@ -6,11 +6,28 @@
 /*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 17:50:42 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/09/07 16:02:56 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/09/08 01:41:44 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <bukoshell.h>
+
+void	free_shell(t_shell *shell, bool full_free)
+{
+	if (!shell)
+		return ;
+	if (shell->line)
+		free(shell->line);
+	if (shell->head)
+		free_tokens(&shell->head);
+	if (shell->root)
+		free_syntax_tree(&shell->root);
+	if (full_free)
+	{
+		free(shell);
+		clear_history();
+	}
+}
 
 int main(void)
 {
@@ -34,6 +51,6 @@ int main(void)
 		shell->status = start_shell(shell);
 	}
 	status = shell->status;
-	free_shell(shell);	
+	free_shell(shell, true);	
 	return (status);
 }
--- src/shell_utils.c
+++ src/shell_utils.c
@@ -3,68 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   shell_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 22:35:58 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/09/07 20:28:06 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/09/08 12:23:28 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <bukoshell.h>
 
-static void update_quote_state(char *str, t_quote_state *state)
-{
-    while (*str)
-    {
-        if (*str == '\'' && !(state->in_double))
-            state->in_single = !(state->in_single);
-        else if (*str == '"' && !(state->in_single))
-            state->in_double = !(state->in_double);
-		str++;
-    }
-}
-
-char *handle_prompt(char *prompt)
+static bool	has_unbalanced_quotes(const char *str)
 {
 	t_quote_state	state;
-    char 			*line;
-    char 			*temp;
-    char 			*combined;
 
 	state = (t_quote_state){false, false};
-    combined = ft_strdup(prompt);
-    if (!combined)
-        return (free(prompt), NULL);
-    free(prompt);
-    update_quote_state(combined, &state);
-    while (state.in_single || state.in_double)
-    {
-        line = readline(PS2);
-        if (!line) 
-            return (free(combined), NULL);
-        update_quote_state(line, &state);
-        temp = ft_vstrjoin(2, "\n", combined, line);
-        free(line);
-        if (!temp)
-            return (free(combined), NULL);
-        free(combined);
-        combined = temp;
-    }
-    return combined;
+	while (*str)
+	{
+		if (*str == '\'' && !(state.in_double))
+			state.in_single = !(state.in_single);
+		else if (*str == '"' && !(state.in_single))
+			state.in_double = !(state.in_double);
+		str++;
+	}
+	return (state.in_single || state.in_double);
 }
 
-void	free_shell(t_shell *shell)
+static void	update_quote_state(char *str, t_quote_state *state)
 {
-	if (!shell)
-		return ;
-	if (shell->line)
-		free(shell->line);
-	if (shell->head)
-		free_tokens(&shell->head);
-	if (shell->root)
-		free_syntax_tree(&shell->root);
-	free(shell);
-	clear_history();
+	while (*str)
+	{
+		if (*str == '\'' && !(state->in_double))
+			state->in_single = !(state->in_single);
+		else if (*str == '"' && !(state->in_single))
+			state->in_double = !(state->in_double);
+		str++;
+	}
+}
+
+static char	*handle_prompt(char *prompt)
+{
+	t_quote_state	state;
+	char			*line;
+	char			*temp;
+	char			*combined;
+
+	state = (t_quote_state){false, false};
+	combined = ft_strdup(prompt);
+	if (!combined)
+		return (free(prompt), NULL);
+	free(prompt);
+	update_quote_state(combined, &state);
+	while (state.in_single || state.in_double)
+	{
+		line = readline(PS2);
+		if (!line)
+			return (free(combined), NULL);
+		update_quote_state(line, &state);
+		temp = ft_vstrjoin(2, "\n", combined, line);
+		free(line);
+		if (!temp)
+			return (free(combined), NULL);
+		free(combined);
+		combined = temp;
+	}
+	return (combined);
 }
 
 t_shell	*init_shell(void)
@@ -79,36 +81,35 @@ t_shell	*init_shell(void)
 	shell->line = NULL;
 	shell->head = NULL;
 	shell->root = NULL;
-	shell->token_mask = 0;
+	shell->prompt_mask = 0;
 	return (shell);
 }
 
-int	start_shell(t_shell *shell)
+int start_shell(t_shell *shell)
 {
-	shell->line = handle_prompt(shell->line);
-	if (!shell->line)
-		return (EXIT_FAILURE);
-	shell->head = create_tokens(shell->line);
-	if (!shell->head)
-		return (EXIT_FAILURE);
-	shell->token_mask = create_token_mask(shell->head); 
-	if (DEBUG_MODE)
+    char *new_line;
+
+    if (has_unbalanced_quotes(shell->line))
+    {
+        new_line = handle_prompt(shell->line);
+        if (!new_line)
+            return (free(shell->line), EXIT_FAILURE);
+        shell->line = new_line;
+    }
+    shell->head = create_tokens(shell->line);
+    if (!shell->head)
+        return (free_shell(shell, false), EXIT_FAILURE);
+    shell->prompt_mask = create_prompt_mask(shell->head);
+    if (DEBUG_MODE)
 	{
-		print_tokens(shell->head, true);
+        print_tokens(shell->head, true);
 		ft_printf("\n");
 	}
-	// shell->root = create_syntax_tree(shell->head);
-	// free_tokens(&shell->head);
-	// if (!shell->root)
-	// 	return (EXIT_FAILURE);
-	// if (DEBUG_MODE)
-	// {
-	// 	ft_printf("\nSyntax Tree:\n");
-	// 	print_syntax_tree(shell->root, ROOT_LEVEL);
-	// 	ft_printf("\n");
-	// }
-	// free_syntax_tree(&shell->root);
-	shell->token_mask = 0;
-	free(shell->line);
-	return (EXIT_SUCCESS);
+    if (!are_valid_tokens(shell->head))
+        return (free_shell(shell, false), 2);
+    // TODO: Add AST creation here ...  
+    // shell->root = create_syntax_tree(shell->head);
+	free_shell(shell, false);
+    shell->prompt_mask = 0;
+    return (shell->status);
 }
