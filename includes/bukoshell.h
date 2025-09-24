408eb4e7222d4e0416ff14d33743155681e6d032
Sean Gadingan <sgadinga@lab1r4s9.42abudhabi.ae>
2025-09-24 21:31:38 +0400

    chore: added os-specific prompts

--- includes/bukoshell.h
+++ includes/bukoshell.h
@@ -3,27 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   bukoshell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
+/*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 17:51:09 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/09/23 17:58:43 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/09/24 20:33:38 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUKOSHELL_H
 # define BUKOSHELL_H
 
+# include <libft.h>
 # include <stdio.h>
 # include <signals.h>
 # include <environ.h>
-# include <libft.h>
 # include <parsing.h>
-# include <readline/readline.h>
 # include <readline/history.h>
+# include <readline/readline.h>
 
-# define PS1 "\e[1mbukoshell\e[m 🞂"
+# if defined(__linux__) 
+# 	define PS1 "\e[1mbukoshell\e[m 🞂"
+# 	define PATH_MAX 4096
+# elif defined(__APPLE__)
+# 	define PS1 "\e[1mbukoshell\e[m %"
+# endif
 
-# define PATH_MAX 4096
 # define DEBUG_MODE true
 
 typedef struct s_shell
