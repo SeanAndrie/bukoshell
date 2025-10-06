39439510cfd7c73f3988ac2d5c9cdbb821f57d4e
SeanAndrie <gseanandrie@gmail.com>
2025-10-06 19:58:50 +0400

    chore: added create_cwd helper for set_prompt

--- src/shell/shell_utils.c
+++ src/shell/shell_utils.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 16:29:15 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/09/25 00:42:19 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/10/03 19:49:30 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,48 +28,50 @@ char	*create_identifier(t_map *map)
 	return (ft_strdup(user->value));
 }
 
-static char	*construct_prompt(char *base_name, char *base_indicator, char *cwd,
-		char *identifier)
+static char	*create_cwd(char *cwd_buffer)
 {
-	char	*temp;
-	char	*prompt;
+	size_t	i;
+	char	*cwd;
+	char	**split;
 
-	temp = ft_vstrjoin(7, NULL, base_name, " [", identifier, " ", cwd, "] ",
-			base_indicator);
-	if (!temp)
+	split = ft_split(cwd_buffer, '/');
+	if (!split)
 		return (NULL);
-	prompt = ft_strjoin(temp, " ");
-	free(temp);
-	if (!prompt)
+	if (!split[0])
+	{
+		free_str_arr(split, -1);
+		return (ft_strdup("/ "));
+	}
+	i = 0;
+	while (split[i + 1])
+		i++;
+	cwd = ft_strdup(split[i]);
+	free_str_arr(split, i);
+	if (!cwd)
 		return (NULL);
-	return (prompt);
+	return (cwd);
 }
 
 char	*set_prompt(t_shell *shell, char *identifier)
 {
-	size_t	i;
+	char	*cwd;
 	char	*prompt;
-	char	**cwd_split;
 	char	**base_split;
 
 	if (!getcwd(shell->cwd, sizeof(shell->cwd)))
 		return (NULL);
-	cwd_split = ft_split(shell->cwd, '/');
-	if (!cwd_split)
+	cwd = create_cwd(shell->cwd);
+	if (!cwd)
 		return (NULL);
-	i = 0;
-	while (cwd_split[i])
-		i++;
 	base_split = ft_split(PS1, ' ');
 	if (!base_split)
-    {
-        free_str_arr(cwd_split, -1);
-        return (NULL);
-    }
-	prompt = construct_prompt(base_split[0], base_split[1], cwd_split[i - 1],
-			identifier);
-	free_str_arr(cwd_split, i);
+		return (NULL);
+	prompt = ft_vstrjoin(8, NULL, base_split[0], " [", identifier, " ", cwd, "] ",
+			base_split[1], " ");
 	free_str_arr(base_split, 2);
+	free(cwd);
+	if (!prompt)
+		return (ft_strdup(PS1));
 	return (prompt);
 }
 
@@ -88,28 +90,13 @@ char	**copy_envp(char **envp)
 	{
 		copy[i] = ft_strdup(envp[i]);
 		if (!copy[i])
-        {
-            free_str_arr(copy, i);
-            return (NULL);
-        }
+		{
+			free_str_arr(copy, i);
+			return (NULL);
+		}
 		i++;
 	}
 	copy[i] = NULL;
 	return (copy);
 }
 
-t_map	*realloc_map(t_map *map, char **envp)
-{
-	t_map	*copy;
-
-	if (!envp)
-		return (NULL);
-	copy = create_map(map->capacity);
-	if (!copy)
-		return (NULL);
-	init_environ(copy, envp);
-	free_map(map);
-	if (!copy)
-		return (NULL);
-	return (copy);
-}
