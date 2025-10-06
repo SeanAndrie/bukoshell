c2cb073775ca228b5bb60a6d9f0c3237046d3c0b
SeanAndrie <gseanandrie@gmail.com>
2025-10-06 19:54:25 +0400

    chore: consolidate prompts in one header for single point of access

--- includes/parsing/prompts.h
+++ includes/parsing/prompts.h
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 02:57:36 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/10/02 03:04:38 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/10/03 19:11:32 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 # define PROMPTS_H
 
 # if defined(__linux__)
-#  define PS1 "\e[1mbukoshell\e[m ❯"
+#  define PS1 "\e[1mbukoshell\e[m ❯ "
 #  define PS2 "∙ "
 #  define PATH_MAX 4096
 # elif defined(__APPLE__)
-#  define PS1 "\e[1mbukoshell\e[m %"
+#  define PS1 "\e[1mbukoshell\e[m % "
 #  define PS2 "> "
 # endif
 
